04fe80b650112de7f30b584484b9436adb706f84
muhamad afif mustafa <muhamadafifmustafa83@gmail.com>
2023-10-17 13:11:42 +0700

    menginstal taliwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'quicksand': ["Quicksand", "sans-serif"]
+      },
+      colors: {
+        pink: {
+          200: '#ADADAD',
+          400: '#FF9191',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
