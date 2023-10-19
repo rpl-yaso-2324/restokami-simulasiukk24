7ab36ef591a5a5d35ef9da787e5b0bfaa2b9c879
Adelia Putri <winkiri1202@gmail.com>
2023-10-19 09:15:48 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Konfigurasi lainnya...
+
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans'],
+      },
+      borderColor: {
+        'custom-orange': '#FFA500', 
+      },
+    },
+  },
+
+  
+}
