ab23fe051b9c59a3d0ace828ad9aad61acf9bb5c
Melandriansyah <jasa123prima@gmail.com>
2023-10-31 07:26:18 -0700

    add

---
+++ login.js
@@ -0,0 +1,27 @@
+const user = {
+    username: "melan",
+    password: "123"
+};
+
+
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const pass = document.getElementById("pass").value;
+    const validName = name == user.username;
+    const validPass = pass == user.password;
+
+    if (name.trim() === "" || pass.trim() === "") {
+        
+        alert('Username & password harus diisi');
+    } else {
+        console.log(validName);
+        console.log(validPass);
+        if (validName === false || validPass === false) {
+        alert('username & password tidak sesuai');
+        document.getElementById("name").value = ""; 
+        document.getElementById("pass").value = "";
+    } else {
+        window.location.href = "home.html";
+    }}
+})
