df5053f887ffbac585e2114b4dd72ef308eb9226
sabilah Mudrikah <sabilahmudrikah@gmail.com>
2023-11-06 09:45:13 +0700

    push

---
+++ src/scripts/login.js
@@ -0,0 +1,31 @@
+const user = {
+	usrname: "sabilah",
+	pass: "123",
+};
+
+/**
+ * login page
+ */
+document
+	.getElementById("loginForm")
+	.addEventListener("submit", function (event) {
+		event.preventDefault();
+		const name = document.getElementById("name").value;
+		const pass = document.getElementById("pass").value;
+		const validName = name == user.usrname;
+		const validPass = pass == user.pass;
+
+		if (name.trim() === "" || pass.trim() === "") {
+			window.alert("Username dan Password harus diisi.");
+		} else {
+			console.log(validName);
+			console.log(validPass);
+			if (validName === false || validPass === false) {
+				window.alert("Username dan Password salah!");
+				document.getElementById("name").value = "";
+				document.getElementById("pass").value = "";
+			} else {
+				location.href = "menu.html";
+			}
+		}
+	});
